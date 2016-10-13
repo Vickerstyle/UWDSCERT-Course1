fc9a4836f4f45a1e38773505e4b43c7123a76742
Vickerstyle <tylervickers89537@gmail.com>
2016-10-13 15:13:20 -0700

    Add files via upload
    Forked the original files before uploading to GH.

---
+++ KMeansNorm_tv.R
@@ -0,0 +1,36 @@
+# KMeansNorm.R
+
+KMeansNorm <- function(observations = sampleObservations, clusterCenters = centersGuess, normD1 = F, normD2 = F)
+{
+  if (normD1)
+  {
+    # Determine mean and standard deviation of 1st dimension in observations
+    # Put code in place of this line
+    # normalize 1st dimension of observations
+    # Put code in place of this line
+    # normalize 1st dimension of clusterCenters
+    # Put code in place of this line
+  }
+  if (normD2)
+  {
+    # Determine mean and standard deviation of 2nd dimension in observations
+    # Put code in place of this line
+    # normalize 2nd dimension of observations
+    # Put code in place of this line
+    # normalize 2nd dimension of clusterCenters
+    # Put code in place of this line
+  }
+  clusterCenters <- KMeans(observations, clusterCenters)
+  if (normD1)
+  {
+    # denormalize in first dimension
+    # Put code in place of this line
+  } 
+  if (normD2)
+  {
+    # denormalize in second dimension
+    # Put code in place of this line
+  } 
+  return(clusterCenters)
+}
+
